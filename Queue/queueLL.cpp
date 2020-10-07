7088ea76bd0ab907767ba853caba72422b652f34
Kunal Mundada <mundadakunal25@gmail.com>
2020-10-07 17:44:03 +0530

    Queue using Linked List

--- Queue/queueLL.cpp
+++ Queue/queueLL.cpp
@@ -4,11 +4,11 @@ using namespace std;
 struct Node
 {
 	int data;
-	Node* link;
+	Node *link;
 };
 
-Node* front = NULL;
-Node* rear = NULL;
+Node *front = NULL;
+Node *rear = NULL;
 
 bool isEmpty()
 {
@@ -20,7 +20,7 @@ bool isEmpty()
 
 void enqueue(int value)
 {
-	Node* ptr = new Node();
+	Node *ptr = new Node();
 	ptr->data = value;
 	ptr->link = NULL;
 	
@@ -63,4 +63,44 @@ void showFront()
 
 void displayQueue()
 {
-	if(is)}
+	if(isEmpty())
+		cout<<"Queue is empty";
+	else 
+	{
+		Node *ptr = front;
+		while(ptr != NULL)
+		{
+			cout<<ptr->data<<" ";
+			ptr = ptr->link;
+		}	
+	}
+}
+
+//Main Function
+int main()
+{
+ int choice, flag=1, value;
+ while( flag == 1)
+ {
+  cout<<"\n1.enqueue 2.dequeue 3.showfront 4.displayQueue 5.exit\n";
+  cin>>choice;
+  switch (choice)
+  {
+  case 1: cout<<"Enter Value:\n";
+          cin>>value;
+          enqueue(value);
+          break;
+  case 2: dequeue();
+          break;
+  case 3: showFront();
+          break;
+  case 4: displayQueue();
+          break;
+  case 5: flag = 0;
+          break;
+  }
+ }
+
+ return 0;
+}
+
